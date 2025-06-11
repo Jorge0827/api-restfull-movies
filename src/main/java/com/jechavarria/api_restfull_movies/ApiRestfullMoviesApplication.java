6844b4be457c3dc05fe09c74b452e4324b9fef13
Jorge Andres Echavarria Pardo <jorgean0827@gmail.com>
2025-06-10 20:07:48 -0500

    Crear proyecto Api RestFull para sistema d egestión de peliculas.

---
+++ src/main/java/com/jechavarria/api_restfull_movies/ApiRestfullMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.jechavarria.api_restfull_movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfullMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfullMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jechavarria/api_restfull_movies/ApiRestfullMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jechavarria.api_restfull_movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfullMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
